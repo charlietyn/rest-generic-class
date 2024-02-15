3996accc9a51822a55e033b64f2076d6f88fd2a4
charlietyn <charlietyn@gmail.com>
2024-02-14 22:42:28 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## REST GENERIC CLASS
+
+Base Class for generic restfull api
+## License
+This library is open-sourced software licensed under the MIT license."# rest-generic-class" 
---
+++ src/BaseFormRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Ronu\RestGenericClass;
+
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class BaseFormRequest extends \Illuminate\Foundation\Http\FormRequest
+{
+
+    use ValidatesRequests;
+
+    public function parseRules($path)
+    {
+        $scenario = $this->getScenario();
+        $rules = include($path);
+        $rules['query'] = $this->query_rules();
+        return $rules[$scenario];
+    }
+
+    public function getScenario()
+    {
+        $scenario = array_key_exists('_scenario', $this->all()) ? $this->all()['_scenario'] : null;
+        if (!$scenario)
+            switch ($this->method()) {
+                case 'POST':
+                    $scenario = 'create';
+                    break;
+                case 'GET'||'DELETE':
+                    $scenario = 'query';
+                    break;
+                case 'PUT' || 'PATCH':
+                    $scenario = 'update';
+                    break;
+                default:
+                    $scenario = 'create';
+            }
+        return $scenario;
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
+
+    private function query_rules()
+    {
+        return [];
+    }
+
+    public function validate_request()
+    {
+        $specific = isset($this->all()['_specific']);
+        $attributes = $this->all();
+        $rules = $specific ? array_intersect_key($this->rules(), $attributes) : $this->rules();
+        $this->validate($this, $rules);
+    }
+
+}
+
